4f5603b9e2eb3ecff19cd39d1321eee59543645b
Sachin <sschin002@gmail.com>
2023-06-16 14:02:04 +0545

    changes

---
+++ index.js
@@ -0,0 +1,15 @@
+// var a='123';
+// console.log(a);
+
+// //using let keyword
+// let b ='45';
+
+// console.log(b);
+// //using const keyword
+// const c = 67;
+// console.log(c);
+// // alert("hello")
+// const age =prompt('age?');
+// console.log(age);
+const a=confirm("are u a student")
+console.log(a);

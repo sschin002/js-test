2f36f66048359eace8b153a0fce5a0b4b5a8674a
Sachin <sschin002@gmail.com>
2023-06-16 21:27:22 +0545

    exercise.1

---
+++ exercise.js
@@ -0,0 +1,17 @@
+//print hello world
+console.log("Hello World!");
+
+//set the value of pi
+const pi = 3.14;
+
+//add two number
+const add= (a,b) =>{
+    if(a!=b){
+        console.log(a+b);
+    }
+    else{
+        console.log (3*a);
+    }
+}
+console.log(add(2,3));
+console.log(add(3,3));

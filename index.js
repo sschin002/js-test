3efe9484c334f022cbb4bed05449c87146d2fb5d
Sachin <sschin002@gmail.com>
2023-06-20 13:53:53 +0545

    array learning

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const person ={
-    firstName :"sachin",
-    lastName :"shrestha",
-    age :22,
-    fullName :()=>{
-        return person.firstName +" "+person.lastName;
-    }
-}
-const fname=person.fullName();
-console.log(fname);
+// const person ={
+//     firstName :"sachin",
+//     lastName :"shrestha",
+//     age :22,
+//     fullName :()=>{
+//         return person.firstName +" "+person.lastName;
+//     }
+// }
+// const fname=person.fullName();
+// console.log(fname);
+//array
+const cars =["volvo", "Buggati", "BMW", 46,{name:"Alto"}];
+// const a= cars[0];
+// console.log(a);
+// const sl= cars[cars.length-1];
+// console.log (sl);
 
+console.log(cars.push("new"));
+console.log(cars);
+cars.pop();
+console.log(cars);

4e1b9be0a35e5334d96e089e740a02be719f0551
Sachin <sschin002@gmail.com>
2023-06-21 14:37:43 +0545

    opening a file

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const http= require("http");
-const dt = require("./myFirstModule");
-const uc =require("upper-case");
+// const http= require("http");
+// const dt = require("./myFirstModule");
+// const uc =require("upper-case");
 
-http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type':'text/html'});
-    const message =uc."hello world";
-    res.end( message + " " +dt.myDateTime());
-}).listen(8081);
-console.log('App running on port 8080');
+// http.createServer((req,res)=>{
+//     res.writeHead(200,{'Content-Type':'text/html'});
+//     const message =uc.upperCase("hello world");
+//     res.end( message + " " +dt.myDateTime());
+// }).listen(8081);
+// console.log('App running on port 8080');
 
+const fs = require('fs');
+const rs = fs.createReadStream('./file.txt');
+rs.on('open',() =>{
+    console.log('the file is opened');
+})

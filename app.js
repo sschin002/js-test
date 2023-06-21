3abd4fa2d17cb6d7cfbfb40fc75e63d22f91f5e6
Sachin <sschin002@gmail.com>
2023-06-21 13:54:07 +0545

    date

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const http= require("http");
+const dt = require("./myFirstModule");
+
 http.createServer((req,res)=>{
     res.writeHead(200,{'Content-Type':'text/html'});
-    res.end('Hello World');
-}).listen(8080);
+    res.end('Hello World its' + " " +dt.myDateTime());
+}).listen(8081);
 console.log('App running on port 8080');
+

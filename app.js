3b5d6b5e02ad726b4164310bd4430f020a3f01c3
Sachin <sschin002@gmail.com>
2023-06-21 14:10:40 +0545

    uppercase module added

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const http= require("http");
 const dt = require("./myFirstModule");
+const uc =require("upper-case");
 
 http.createServer((req,res)=>{
     res.writeHead(200,{'Content-Type':'text/html'});
-    res.end('Hello World its' + " " +dt.myDateTime());
+    const message =uc."hello world";
+    res.end( message + " " +dt.myDateTime());
 }).listen(8081);
 console.log('App running on port 8080');
 

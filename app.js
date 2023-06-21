d8e9897d1c1c501521c407fb4a803e9ae1c71c5d
Sachin <sschin002@gmail.com>
2023-06-21 14:48:01 +0545

    displaying a content of a file

--- app.js
+++ app.js
@@ -9,8 +9,14 @@
 // }).listen(8081);
 // console.log('App running on port 8080');
 
+//file opened
 const fs = require('fs');
 const rs = fs.createReadStream('./file.txt');
+
 rs.on('open',() =>{
     console.log('the file is opened');
+})
+
+rs.on("data",(data) =>{
+    console.log(data.toString());
 })
